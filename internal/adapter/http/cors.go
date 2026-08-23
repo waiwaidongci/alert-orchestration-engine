d8c42043fdbb7af41d880b517fccf8f040017464
waiwaidongci <786958579@qq.com>
2026-08-23 16:13:58 +0800

    test: add acceptance checks

---
+++ internal/adapter/http/cors.go
@@ -0,0 +1,16 @@
+package httpadapter
+
+import "net/http"
+
+func CORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, X-Request-ID")
+		w.Header().Set("Access-Control-Allow-Methods", "GET,POST,DELETE,OPTIONS")
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			w.Header().Set("X-Preflight", "done")
+		}
+		next.ServeHTTP(w, r)
+	})
+}
